029c763c7201573ea0e2f60b24908f07046c4327
Dima <dmitryg@hp.com>
2018-03-18 11:44:05 +0200

    Final commit

---
+++ HelloWorldClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldClient
+{
+    class Program
+    {
+        static HelloWorldServiceClient client = new HelloWorldServiceClient();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(client.GetMessage("DIIIIMMMMAAA!"));
+        }
+
+        static async Task<string> GetStrAsync()
+        {
+            var s = await client.GetMessageAsync("Dimmmma!");
+
+            return s;
+        }
+    }
+}
